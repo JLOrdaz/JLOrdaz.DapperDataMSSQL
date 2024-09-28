57d82c85068fca615c8e3b95fe74118ea72af27c
Jose Luis Ordaz <jlordsan@hotmail.com>
2024-09-28 19:51:04 -0400

    Rename BD to DB Add readme.md Add Icon

--- SQLDataAccess.cs
+++ SQLDataAccess.cs
@@ -7,19 +7,19 @@ namespace JLOrdaz.DapperDataMSSQL;
 
 public class SQLDataAccess(IConfiguration configuration) : ISQLDataAccess
 {
-    public async Task<IEnumerable<T>> LoadData<T, U>(string storeProcedure, U parameters, string connectionId = "BD")
+    public async Task<IEnumerable<T>> LoadData<T, U>(string storeProcedure, U parameters, string connectionId = "DB")
     {
         using IDbConnection conex = new SqlConnection(configuration.GetConnectionString(connectionId));
         return await conex.QueryAsync<T>(storeProcedure, parameters, commandType: CommandType.StoredProcedure) ?? Enumerable.Empty<T>();
     }
 
-    public async Task<T?> LoadFirst<T, U>(string storeProcedure, U parameters, string connectionId = "BD")
+    public async Task<T?> LoadFirst<T, U>(string storeProcedure, U parameters, string connectionId = "DB")
     {
         using IDbConnection conex = new SqlConnection(configuration.GetConnectionString(connectionId));
         return await conex.QueryFirstOrDefaultAsync<T>(storeProcedure, parameters, commandType: CommandType.StoredProcedure);
     }
 
-    public async Task SaveData<T>(string storeProcedure, T parameters, string connectionId = "BD")
+    public async Task SaveData<T>(string storeProcedure, T parameters, string connectionId = "DB")
     {
         using IDbConnection conex = new SqlConnection(configuration.GetConnectionString(connectionId));
         await conex.ExecuteAsync(storeProcedure, parameters, commandType: CommandType.StoredProcedure);
