d0827170f2695f1aa3d2944f6eaca70d21ca7b2c
Jose Luis Ordaz <jlordsan@hotmail.com>
2024-09-28 19:11:37 -0400

    Version inicial

---
+++ ISQLDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace JLOrdaz.DapperDataMSSQL;
+
+public interface ISQLDataAccess
+{
+    Task<IEnumerable<T>> LoadData<T, U>(string storeProcedure, U parameters, string connectionId = "DB");
+    Task<T?> LoadFirst<T, U>(string storeProcedure, U parameters, string connectionId = "DB");
+    Task SaveData<T>(string storeProcedure, T parameters, string connectionId = "DB");
+}
---
+++ SQLDataAccess.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using Dapper;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace JLOrdaz.DapperDataMSSQL;
+
+public class SQLDataAccess(IConfiguration configuration) : ISQLDataAccess
+{
+    public async Task<IEnumerable<T>> LoadData<T, U>(string storeProcedure, U parameters, string connectionId = "BD")
+    {
+        using IDbConnection conex = new SqlConnection(configuration.GetConnectionString(connectionId));
+        return await conex.QueryAsync<T>(storeProcedure, parameters, commandType: CommandType.StoredProcedure) ?? Enumerable.Empty<T>();
+    }
+
+    public async Task<T?> LoadFirst<T, U>(string storeProcedure, U parameters, string connectionId = "BD")
+    {
+        using IDbConnection conex = new SqlConnection(configuration.GetConnectionString(connectionId));
+        return await conex.QueryFirstOrDefaultAsync<T>(storeProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task SaveData<T>(string storeProcedure, T parameters, string connectionId = "BD")
+    {
+        using IDbConnection conex = new SqlConnection(configuration.GetConnectionString(connectionId));
+        await conex.ExecuteAsync(storeProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+}
